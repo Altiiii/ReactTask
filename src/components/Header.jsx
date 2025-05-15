92304b410139056c5baafb845e7bc065f419e0b4
Altin ismaili <ai30962@seeu.edu.mk>
2025-05-15 15:32:03 +0200

    REAC-1 Header First Part Done

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <header className="fixed top-0 left-0 right-0 h-16 bg-gray-800 shadow-md flex items-center px-4 z-40" >
+            <h1 className="text-xl font-bold text-white">MyFinances</h1>
+        </header>
+    );
+}
